8ba2a5eb4bb23773c83b11b09a8b46ef7ce2803b
MelindaMezinca <melinda.mezinca@yahoo.com>
2019-04-01 17:39:04 +0300

    Tour of heroes

---
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
+
+@Component({
+  selector: "app-heroes", // the component's CSS element selector
+  templateUrl: "./heroes.component.html", // the location of the component's template file
+  styleUrls: ["./heroes.component.css"] // the location of the component's private CSS styles
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
